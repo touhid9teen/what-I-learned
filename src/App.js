a9dff6aa5c5550340fead51bf0c2c9f08b43992f
Touhidul Islam <toouhidul@gmail.com>
2023-12-27 23:33:40 +0600

    Added for first stage learning

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import JavaScript from "./Component/JavaScript/JavaScript";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <JavaScript />
+    </>  
   );
 }
 
---
+++ src/Component/JavaScript/JavaScript.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Style.css'
+const JavaScript=() => {
+  return (
+    <>
+        <h1 className='heading'>Arrow Function</h1>
+        <h2 className='heading'>- Basic Syntax</h2>
+        <p className='heading'>{`const functionName = (parameter1, parameter2, parameter3) => { }`}</p>
+        <h1 className='heading'>JavaScript Work and Execution</h1>
+        <div className='heading'>
+            Every thing in JavaScript is an object.
+            JavaScript is a single threaded language
+             that can be non-blocking.
+        </div>
+    </>
+  )
+}
+export default JavaScript;
+
